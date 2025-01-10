cc0d60e8346571d94327fbd831f1141860c06088
Emmm-07 <elemem23@gmail.com>
2025-01-10 23:19:53 +0800

    intergrated backend and frontend as submodules

--- frontend/config.jsx
+++
@@ -1,7 +0,0 @@
-//This is to declare global variables
-
-//for testing
-export const hostUrl = 'http://127.0.0.1:8000/'
-
-//for deployment
-// export const hostUrl = 'https://blogs-djangoapi.onrender.com/'
